ba6e25eedd484dcb7d181c45a968496b94e04d6c
Dmitry Shorokhov <vip.shoroch@gmail.com>
2021-04-25 16:11:29 +0300

    backend register impl

--- chat-server/src/main/java/ru/miet/example/grpc/chat/entity/ChatUser.java
+++ chat-server/src/main/java/ru/miet/example/grpc/chat/entity/ChatUser.java
@@ -14,6 +14,7 @@ import javax.persistence.*;
 @Entity
 public class ChatUser {
 
+    @org.springframework.data.annotation.Id
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
